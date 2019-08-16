0f095e1337d6521efeef3388b9c88d3dc5cdb6f8
kyle <kyle@freshcode.me>
2019-08-16 13:13:48 +0900

    [feature] 프로젝트 구성 - vue-cli 3.x 를 이용하여 프로젝트 구성 - scss 의존성 추가 - vuex / router 의존성 추가

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # vue-weather-v2
-vue-wearther v2
+
+- vue-weather 앱
+
+```
+vue-cli : 3.8.3
+프로젝트 구성
+- babel / linter, formatter (수동 기본 구성)
+
+scss 구성
+npm i --save-dev node-sass sass-loader
+
+vue router / vuex 구성
+npm i --save vue-router vuex
+
+vue ui
+runtimeCompiler: true 설정
+
+
+```
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Run your tests
+```
+npm run test
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <p class="scss-test">scss test</p>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="scss">
+
+  $testColor : #2c3e50;
+  $fontSize : 10;
+  $bgColor : #2c3e50;
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: $testColor;
+  margin-top: 60px;
+}
+
+  .scss-test {
+    font-size: $fontSize;
+    background-color: $bgColor;
+  }
+
+</style>
